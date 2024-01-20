be925e0f079ede5c8829be9c1f92c5b3de3665f6
kgy1008 <gayeon1008@g.skku.edu>
2024-01-20 22:23:42 +0900

    connect database

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient("CONNECTION STRING")
-db = client['DB NAMEEEEEE']
+client = MongoClient("mongodb+srv://libra001008:S9G7TwCghM9rgi9z@cluster0.pchoafe.mongodb.net/")
+db = client['profile']
+user_collection = db['user']
 
 @app.route('/')
 def home():
     return 'Hello, Flask!'
+
+@app.route('/get_data')
+def get_data():
+    # Example: retrieve data from MongoDB
+    data = db.user_collection.find()
+    return jsonify({"data": list(data)})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
