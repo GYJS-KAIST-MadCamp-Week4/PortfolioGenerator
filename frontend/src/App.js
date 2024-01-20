2d6fbf6c4cc69b476021ef0227e1b6fe63c21f7d
kgy1008 <gayeon1008@g.skku.edu>
2024-01-20 17:08:18 +0900

    start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import Login from './components/Login';
 import Start from './components/Start';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Start/>
-      <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
     
   );
 }
--- frontend/src/components/Start.js
+++ frontend/src/components/Start.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './Start.css'; 
 
 const Start = () => {
   const [showInitialText, setShowInitialText] = useState(true);
   const [showGradientBackground, setShowGradientBackground] = useState(false); // 상태 변수 정의
   const [showFinalText, setShowFinalText] = useState(false);
+  const [showClickToEnter, setShowClickToEnter] = useState(false);
   const [typedText, setTypedText] = useState('');
   const text = "HAEJWO;";
 
@@ -18,13 +20,14 @@ const Start = () => {
               return typedText + text[nextIndex];
             } else {
               clearInterval(intervalId);
+              setShowClickToEnter(true);
               return typedText;
             }
           });
         }, 550);
         
         return () => clearInterval(intervalId);
-      }, 1000); 
+      }, 300); 
   
       return () => clearTimeout(timeoutId);
     }
@@ -33,12 +36,12 @@ const Start = () => {
   useEffect(() => {
     const initialTextTimer = setTimeout(() => {
       setShowInitialText(false);
-      setShowGradientBackground(true); // 여기서 그라데이션 배경을 활성화
+      setShowGradientBackground(true); 
     }, 1000); 
 
     const gradientTimer = setTimeout(() => {
-      setShowFinalText(true); // 여기서 최종 텍스트를 활성화
-    }, 2000); // 초기 이미지가 흐려진 후 1초 뒤에 최종 텍스트가 나타남
+      setShowFinalText(true); 
+    }, 2000); 
 
     return () => {
       clearTimeout(initialTextTimer);
@@ -49,12 +52,13 @@ const Start = () => {
   return (
     <div className="start-page">
       {showInitialText && <div className="initial-text">개발자 포트폴리오 사이트 누가 대신 만들어줘!</div>}
-      <div className={`background-image ${showGradientBackground ? 'show-gradient' : ''} ${!showInitialText ? 'fade' : ''}`} />
-      {showFinalText && 
+      <div className={`background-image ${!showInitialText ? 'fade' : ''}`} />
+      {showFinalText && showGradientBackground &&
         <div className={`final-text ${showFinalText ? 'show' : ''}`}>
           {typedText}
           <span className='cursor'/>
         </div>}
+      {showClickToEnter && <Link to="/login"><div className="click-to-enter">click to enter</div></Link> }
     </div>
   );
 };
